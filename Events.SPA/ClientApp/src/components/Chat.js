f319f40be95c950c35d050e3d90bc9576274c62a
Rachel McCaughley <rachelmccaughley@rachels-air.home>
2023-03-09 20:30:44 +0000

    second commit

---
+++ Events.SPA/ClientApp/src/components/Chat.js
@@ -0,0 +1,37 @@
+﻿import React, { useEffect } from 'react';
+import Talk from 'talkjs';
+
+function Chat() {
+    useEffect(() => {
+        // Initialize TalkJS SDK
+        Talk.ready.then(() => {
+            const me = new Talk.User({
+                id: "1234",
+                name: "John",
+                email: "john@example.com",
+                photoUrl: "https://demo.talkjs.com/img/alice.jpg",
+                welcomeMessage: "Hey there! How can I help you today?",
+                role: "user"
+            });
+
+            // Authenticate user with TalkJS
+            const session = new Talk.Session({
+                appId: "tq5UXJMd",
+                me: me
+            });
+
+            // Create chatbox
+            const conversation = session.getOrCreateConversation("CONVERSATION_ID");
+            const chatbox = session.createChatbox(conversation);
+            chatbox.mount(document.getElementById("talkjs-container"));
+        });
+    }, []);
+
+    return (
+        <div id="talkjs-container" style={{ height: '500px' }}>
+            Loading chat...
+        </div>
+    );
+}
+
+export default Chat;
--- Events.SPA/ClientApp/src/components/Home.js
+++ Events.SPA/ClientApp/src/components/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import  Chat  from './Chat';
 export class Home extends Component {
   static displayName = Home.name;
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+            <h1>Hello, world!</h1>
+        <Chat></Chat>
         <p>Welcome to your new single-page application, built with:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
